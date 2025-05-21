e519a2e03aacdc4e57dc2ede0c9808af3996db33
RafaelBlanco00 <rafaelblanco00@gmail.com>
2025-05-20 20:14:12 -0500

    Ultima Conf. - Proyecto Personal
    VR Escena Base, proyecto personal:
    - XR Origin
    - Tunelling Vignette
    - Teleportation
    - Locomotion.
    - RightHandController.cs

--- RBlancoP1/Assets/Scripts/RightHandController.cs
+++ RBlancoP1/Assets/Scripts/RightHandController.cs
@@ -1,18 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class RightHandController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public XRRayInteractor XRRayInteractor_RightHand_Grab;
+    public XRRayInteractor XRRayInteractor_RightHand_Teleport;
+
+    public InputActionReference PalancaControlDerecho;
+
+    private void Start()
     {
-        
+        XRRayInteractor_RightHand_Grab.enabled = true;
+        XRRayInteractor_RightHand_Teleport.enabled = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PalancaArribaPresionada(InputAction.CallbackContext contexto)
     {
-        
+        XRRayInteractor_RightHand_Grab.enabled = false;
+        XRRayInteractor_RightHand_Teleport.enabled = true;
+    }
+
+    public void PalancaArribaLiberada(InputAction.CallbackContext contexto)
+    {
+        Invoke("PalancaArribaLiberada_Invoke", 0.005f);
+    }
+
+    public void PalancaArribaLiberada_Invoke()
+    {
+        XRRayInteractor_RightHand_Grab.enabled = true;
+        XRRayInteractor_RightHand_Teleport.enabled = false;
+    }
+
+    private void OnEnable()
+    {
+        PalancaControlDerecho.action.performed += PalancaArribaPresionada;
+        PalancaControlDerecho.action.canceled += PalancaArribaLiberada;
+    }
+
+    private void OnDisable()
+    {
+        PalancaControlDerecho.action.performed -= PalancaArribaPresionada;
+        PalancaControlDerecho.action.canceled -= PalancaArribaLiberada;
     }
 }
